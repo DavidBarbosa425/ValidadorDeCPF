580deb5fc3dd68e2a0f1c6ead8ec82293cb385e5
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-09-21 00:13:00 -0300

    update

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-   let cpfLimpo
-function limpaCPF(cpfEnviado) {
-    cpfLimpo = cpfEnviado.replace(/\D+/g, '');
-    validaCPF(cpfLimpo);
-};
+class ValidadordeCPF {
+    constructor(cpfEnviado) {
+        this.cpfEnviado = cpfEnviado;
+        this.cpfLimpo;
+        this.total;
+    }
 
-function validaCPF(cpfLimpo) {
-    if(cpfLimpo.length !== 11) return console.log('CPF inválido');
-    if(cpfLimpo[0].repeat(cpfLimpo.length) === cpfLimpo) return console.log('CPF inválido');
-    cpfSemDigitos(cpfLimpo);
+    limpaCPF() {
+        this.cpfLimpo = this.cpfEnviado.replace(/\D+/g, '');
+        this.validaCPF(this.cpfLimpo);
+    };
+    
+    validaCPF(cpfLimpo) {
+        if(this.cpfLimpo.length !== 11) return console.log('CPF inválido, verifique se foi inserido os 11 digitos');
+        if(this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo) return console.log('CPF inválido, números repetidos');
+        this.cpfSemDigitos(this.cpfLimpo);
+    }
+    
+    cpfSemDigitos(cpfLimpo) {
+        this.cpfLimpo10dig = this.cpfLimpo.slice(0, -2);
+        this.calculaDigito1CPF(this.cpfLimpo10dig);
+    }
+    
+    calculaDigito1CPF(cpfLimpo10dig) {
+        
+           this.total = Number(this.cpfLimpo10dig[0] * 10);
+           this.total += Number(this.cpfLimpo10dig[1] * 9);
+           this.total += Number(this.cpfLimpo10dig[2] * 8);
+           this.total += Number(this.cpfLimpo10dig[3] * 7);
+           this.total += Number(this.cpfLimpo10dig[4] * 6);
+           this.total += Number(this.cpfLimpo10dig[5] * 5);
+           this.total += Number(this.cpfLimpo10dig[6] * 4);
+           this.total += Number(this.cpfLimpo10dig[7] * 3);
+           this.total += Number(this.cpfLimpo10dig[8] * 2);
+           this.resto = this.total % 11
+           this.dig1 = 11 - this.resto
+           this.cpf10dig = String(this.cpfLimpo10dig + this.dig1)
+           this.calculaDigito2CPF(this.cpf10dig) 
+    }
+    
+    calculaDigito2CPF(cpf10dig) {
+        
+           this.total = Number(this.cpf10dig[0] * 11);
+           this.total += Number(this.cpf10dig[1] * 10);
+           this.total += Number(this.cpf10dig[2] * 9);
+           this.total += Number(this.cpf10dig[3] * 8);
+           this.total += Number(this.cpf10dig[4] * 7);
+           this.total += Number(this.cpf10dig[5] * 6);
+           this.total += Number(this.cpf10dig[6] * 5);
+           this.total += Number(this.cpf10dig[7] * 4);
+           this.total += Number(this.cpf10dig[8] * 3);
+           this.total += Number(this.cpf10dig[9] * 2);
+           this.resto = this.total % 11;
+           this.dig2 = 11 - this.resto
+           this.cpfVerificado = String(this.cpf10dig + this.dig2)
+           this.respondeSeCPFevalido(this.cpfVerificado)
+    }
+    
+    respondeSeCPFevalido(cpfVerificado) {
+        if(this.cpfLimpo === this.cpfVerificado) return console.log('CPF valido')
+        return console.log('CPF inválido')     
+    }
 }
-
-function cpfSemDigitos(cpfLimpo) {
-    cpfLimpo = cpfLimpo.slice(0, -2);
-    calculaDigito1CPF(cpfLimpo);
-}
-
-function calculaDigito1CPF(cpfLimpo) {
-    let total;
-       total = Number(cpfLimpo[0] * 10);
-       total += Number(cpfLimpo[1] * 9);
-       total += Number(cpfLimpo[2] * 8);
-       total += Number(cpfLimpo[3] * 7);
-       total += Number(cpfLimpo[4] * 6);
-       total += Number(cpfLimpo[5] * 5);
-       total += Number(cpfLimpo[6] * 4);
-       total += Number(cpfLimpo[7] * 3);
-       total += Number(cpfLimpo[8] * 2);
-       let resto = total % 11
-       let dig1 = 11 - resto
-       let cpf10dig = String(cpfLimpo + dig1)
-       calculaDigito2CPF(cpf10dig) 
-}
-
-function calculaDigito2CPF(cpf10dig) {
-    let total;
-       total = Number(cpf10dig[0] * 11);
-       total += Number(cpf10dig[1] * 10);
-       total += Number(cpf10dig[2] * 9);
-       total += Number(cpf10dig[3] * 8);
-       total += Number(cpf10dig[4] * 7);
-       total += Number(cpf10dig[5] * 6);
-       total += Number(cpf10dig[6] * 5);
-       total += Number(cpf10dig[7] * 4);
-       total += Number(cpf10dig[8] * 3);
-       total += Number(cpf10dig[9] * 2);
-       let resto = total % 11;
-       let dig2 = 11 - resto
-       let cpfVerificado = String(cpf10dig + dig2)
-       respondeSeCPFevalido(cpfVerificado)
-}
-
-function respondeSeCPFevalido(cpfVerificado) {
-    if(cpfLimpo === cpfVerificado) return console.log('CPF valido')
-    return console.log('CPF inválido')
-}
-
-limpaCPF('381.498.248-71')
+   
+let validaCPF = new ValidadordeCPF('371.498.248-71');
+validaCPF.limpaCPF()
 
